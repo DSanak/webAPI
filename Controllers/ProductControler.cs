9b94f48d996c7ec0b8c99313558dcfc7e4b53316
Dominik Sanak <dominik.sanak159@gmail.com>
2022-07-14 19:17:07 +0200

    Learn about WebAPI

---
+++ Controllers/ProductControler.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace PdIwtA_1a.ASP.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private static readonly List<Product> _products = new List<Product>
+        {
+            new Product(Guid.NewGuid(), "Ananas", true),
+            new Product(Guid.NewGuid(), "Banan", false),
+            new Product(Guid.NewGuid(), "Kiwi", true)
+        };
+
+        [HttpGet]
+        public IEnumerable<Product> GetAll()
+        {
+            return _products;
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult Get(Guid id)
+        {
+            var product = _products.FirstOrDefault(x => x.Id == id);
+
+            if (product is null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        [HttpPost("[action]")]
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        [HttpPut("action")]
+        public IActionResult Update(Product product)
+        {
+            var produktid = _products.FindIndex(x => x.Id == product.Id);
+
+            if (product is null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        [HttpDelete("[action]")]
+        public IActionResult Delete(Guid id)
+        {
+            var product = _products.FirstOrDefault(x => x.Id == id);
+
+            if (product is null)
+                return NotFound();
+
+            _products.Remove(product);
+            return Ok();
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class Product
+    {
+
+        public Product(Guid id, string name, bool isAvalible)
+        {
+            Id = id;
+            Name = name;
+            IsAvalible = isAvalible;
+        }
+
+        public Guid Id { get; }
+        public string Name { get; }
+        public bool IsAvalible { get; }
+
+
+    }
+}
